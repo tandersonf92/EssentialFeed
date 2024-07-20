15dcacbbd043c1e19ce16901d4299d2a80e156be
tandersonf <anderson.oliveira@cora.com.br>
2024-07-20 10:55:36 -0300

    Uptade makeItem function removing reduce, using compactMapValues now

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -101,9 +101,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             "description": description,
             "location": location,
             "image": imageURL.absoluteString
-        ].reduce(into: [String: Any]()) { (acc, element) in
-            if let value = element.value { acc[element.key] = value }
-        }
+        ].compactMapValues{ $0 }
 
         return (item, json)
     }
