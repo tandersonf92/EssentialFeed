775e96fa6454301c13d40a7dd0ded70f15618ff0
tandersonf <anderson.oliveira@cora.com.br>
2024-09-02 11:35:02 -0300

    Moves UTLSessionHTTPCLient to its own file in production

---
+++ EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -0,0 +1,23 @@
+public final class URLSessionHTTPClient: HTTPClient {
+
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    private struct UnexpectedValuesRepresentation: Error {}
+
+    public func get(from url: URL, completion: @escaping (HTTClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            }
+            else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
